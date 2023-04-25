98577a97979e0fc9bfdaa8518eb44f257604a5f6
sugoiJG <jasongolliet@icloud.com>
2023-04-24 21:42:04 -0700

    finished making my frog moveable, and added logs

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ const resultDisplay = document.querySelector('#result')
 
 const startPauseButton = document.querySelector('#start-pause-button')
 
-const squares = document.querySelector('.grid div')
+const squares = document.querySelectorAll('.grid div')
 
-let currentIndex = 0
+console.log(squares)
+let currentIndex = 76
+const width = 9
 
-const moveFrog = () => {
-    console.log('moved')
+const moveFrog = (e) => {
+    squares[currentIndex].classList.remove('frog')
+
+
+    switch(e.key) {
+        case 'ArrowLeft' :
+            console.log('move left')
+            if (currentIndex % width !== 0) currentIndex -=1
+            break
+        case 'ArrowRight' :
+            console.log('move right')
+            if (currentIndex % width < width - 1) currentIndex += 1
+            break
+        case 'ArrowUp' :
+            console.log('move up')
+            if (currentIndex - width >=0) currentIndex -= width
+            break
+        case 'ArrowDown' :
+            console.log('move down')
+            if (currentIndex + width < width * width) currentIndex += width
+            break
+
+    }
 
     squares[currentIndex].classList.add('frog')
 
