d562226e62a315ee897fe9f01c48adae640b8ae9
sugoiJG <jasongolliet@icloud.com>
2023-04-24 19:19:43 -0700

    added frog to the board

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ const startPauseButton = document.querySelector('#start-pause-button')
 
 const squares = document.querySelector('.grid div')
 
+let currentIndex = 0
+
 const moveFrog = () => {
     console.log('moved')
+
+    squares[currentIndex].classList.add('frog')
 
 }
 
